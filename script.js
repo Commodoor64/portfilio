0d3676928e27bbd74508c50f3460489ec37ef710
Commodoor64 <61077455+Commodoor64@users.noreply.github.com>
2024-11-11 10:24:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+window.onload = function(){ 
+    document.getElementById("catalogue").onclick = function () {
+        location.href = "catalogue.html";
+    };
+    document.getElementById("home").onclick = function () {
+        location.href = "index.html";
+    };
+    document.getElementById("yiddish_card").onclick = function () {
+        location.href = "yiddishcard.html";
+    };
+    document.getElementById("misczine").onclick = function () {
+        location.href = "MISC1.html";
+    };
+    document.getElementById("genesis").onclick = function () {
+        location.href = "genesis.html";
+    };
+    document.getElementById("disposessed").onclick = function () {
+        location.href = "disposessed.html";
+    };
+};
